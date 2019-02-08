703a03f54f7ba021a3bfa52e2fec7013acdb6d3a
Ross Altman <raltman@INARI-1002.local>
2019-02-08 12:01:43 -0500

    Updated docs.

--- build_docs.sh
+++ build_docs.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# 1) Install sphinx (conda install -c anaconda sphinx, or pip install sphinx)
+# 2) mkdir docs && cd docs
+# 3) Run sphinx-quickstart
+# 4) Run this script (bash ./build_docs.sh)
+
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 sphinx-apidoc -f -o ${DIR}/docs/source/ ${DIR}
